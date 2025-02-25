876b5e0c8f5976cad862cf083367e3ddbb01001a
HoKirby <lakevely27@gmail.com>
2025-02-25 22:06:29 +0900

    chore: create and segment common packages
    common 패키지 내부에 config, exception, filter, handler 패키지 생성 및 세분화

---
+++ src/main/java/org/example/expert/common/exception/AdminPrivilegeException.java
@@ -0,0 +1,7 @@
+package org.example.expert.common.exception;
+
+public class AdminPrivilegeException extends RuntimeException {
+    public AdminPrivilegeException(String message) {
+        super(message);
+    }
+}
