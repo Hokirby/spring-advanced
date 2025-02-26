08dfe4d6992219d2a122ce198ed25a9629a5384d
HoKirby <lakevely27@gmail.com>
2025-02-27 06:26:41 +0900

    refactor: 5 Remove unnecessary nullSafeEquals methods
    null 체크를 이미 진행했으므로 nullSafeEquals() 메서드 사용이 불필요해 제거

--- src/main/java/org/example/expert/domain/manager/service/ManagerService.java
+++ src/main/java/org/example/expert/domain/manager/service/ManagerService.java
@@ -81,17 +81,17 @@ public class ManagerService {
         Todo todo = todoRepository.findById(todoId)
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));
 
-        if (todo.getUser() == null || !ObjectUtils.nullSafeEquals(user.getId(), todo.getUser().getId())) {
+        if (todo.getUser() == null || ! user.getId().equals(todo.getUser().getId())) {
             throw new InvalidRequestException("해당 일정을 만든 유저가 유효하지 않습니다.");
         }
 
         Manager manager = managerRepository.findById(managerId)
                 .orElseThrow(() -> new InvalidRequestException("Manager not found"));
 
-        if (!ObjectUtils.nullSafeEquals(todo.getId(), manager.getTodo().getId())) {
+        if (!todo.getId().equals(manager.getTodo().getId())) {
             throw new InvalidRequestException("해당 일정에 등록된 담당자가 아닙니다.");
         }
 
-        managerRepository.delete(manager);
+        managerRepository.deleteById(managerId);
     }
 }
